eecb49b4a6f94d185ed0cac67ee4a660a2af3826
josephbill <josephbill00@gmail.com>
2025-04-14 17:03:12 +0300

    added composable screens for instagram post card

--- app/src/main/java/com/example/starterapp/ConstraintLayoutActivity.kt
+++ app/src/main/java/com/example/starterapp/ConstraintLayoutActivity.kt
@@ -5,22 +5,36 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Send
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -39,6 +53,7 @@ import androidx.compose.ui.unit.dp
 class ConstraintLayoutActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         // inside this function we load our composables
         setContent {
             // call the material theme from google material design
@@ -49,7 +64,7 @@ class ConstraintLayoutActivity : ComponentActivity(){
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background ) {
 //                    host other items buttons, images , text
-                      InstagramPostCard()
+                    InstagramPostCard()
                 }
             }
         }
@@ -63,29 +78,82 @@ class ConstraintLayoutActivity : ComponentActivity(){
     // timestamp
     @Composable
     fun InstagramPostCard(){
+        // variable to maintain the like and unlike states
+        val isLiked  = remember { mutableStateOf(false) }
         // card element
         Card(shape = RoundedCornerShape(12.dp),
             elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
             modifier = Modifier.fillMaxWidth().padding(16.dp)) {
            // A layout composable that places its children in a vertical sequence.
             Column {
-                      // header section
+                // header section
                 // A layout composable that places its children in a horizontal sequence.
                 Row (verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier.fillMaxWidth().padding(8.dp)) {
-                         // image composable = profile pic
-                        Image(painter = painterResource(id = R.drawable.image1) ,
-                            contentDescription = "Profile Picture",
-                            modifier = Modifier.size(40.dp).clip(CircleShape)
-                                .border(1.dp, Color.Gray, CircleShape),
-                            contentScale = ContentScale.Crop)
+//                   username and image
+                    // image composable = profile pic
+                    Image(painter = painterResource(id = R.drawable.image1) ,
+                        contentDescription = "Profile Picture",
+                        modifier = Modifier.size(40.dp).clip(CircleShape)
+                            .border(1.dp, Color.Gray, CircleShape),
+                        contentScale = ContentScale.Crop)
                     //spacer
-                        Spacer(modifier = Modifier.width(8.dp))
+                    Spacer(modifier = Modifier.width(8.dp))
                     // Text
-                       Text(text="username" , fontWeight = FontWeight.Bold ,
-                           modifier = Modifier.weight(1f))
+                    Text(text="username" , fontWeight = FontWeight.Bold ,
+                        modifier = Modifier.weight(1f))
 
-                      }
+                }
+//                // instagram image
+                  // BOX composable : BOX LIKE CONTAINER
+                Box(modifier = Modifier.fillMaxWidth().height(300.dp)) {
+                        Image(painter= painterResource(id = R.drawable.image2),
+                              contentDescription = "Post Image",
+                              modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.Crop
+                        )
+                }
+                // Action Buttons
+                    Row(modifier = Modifier.fillMaxWidth().padding(8.dp)) {
+//                     // Iconbutton composable: like button
+                        IconButton(onClick = { isLiked.value = !isLiked.value}) {
+                            Icon(
+                                if(isLiked.value) Icons.Filled.Favorite
+                                else Icons.Outlined.FavoriteBorder,
+                                contentDescription = "Like Button",
+                                tint = if(isLiked.value) Color.Red
+                                else MaterialTheme.colorScheme.onSurface
+                            )
+                        }
+                        // comment
+                        IconButton(onClick = {}) {
+                            Icon(Icons.Outlined.CheckCircle, contentDescription = "Comment")
+                        }
+                        // share
+                        IconButton(onClick = {}) {
+                            Icon(Icons.Outlined.Share, contentDescription = "Share")
+                        }
+                        //spacer
+                        Spacer(modifier = Modifier.weight(1f))
+                        // save
+                        IconButton(onClick = {}) {
+                            Icon(Icons.Outlined.Star, contentDescription = "Save")
+                        }
+
+                    }
+                    // text container for the likes count
+                    Text(text= "${if (isLiked.value) 1 else 0} likes",
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(start = 16.dp, bottom = 4.dp))
+
+                    // caption
+                    Row (modifier = Modifier.padding(horizontal= 16.dp, vertical = 4.dp)) {
+                        Text(text="username", fontWeight = FontWeight.Bold)
+                        Text(text="This is a sample instagram caption!.")
+
+                    }
+
+
                 }
 
         }
